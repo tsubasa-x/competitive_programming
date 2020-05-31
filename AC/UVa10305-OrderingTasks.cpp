18e14e5ca6cfc4618a63133c94bd57aae3733d48
tsubasa-x <56198419+tsubasa-x@users.noreply.github.com>
2020-05-31 20:07:58 +0800

    Add files via upload

---
+++ AC/UVa10305-OrderingTasks.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+//ofstream file;
+
+const int maxn = 100 + 10;
+int n, m;
+int t;
+bool graph[maxn][maxn];
+int visit[maxn];
+int order[maxn];
+
+bool dfs(int u){
+    visit[u] = -1;
+    for(int i = 1; i <= n; i++){
+        if(graph[u][i]){
+            if(visit[i] < 0) return false;
+            if(!visit[i] && !dfs(i)) return false;
+        }
+    }
+    visit[u] = 1;
+    order[--t] = u;
+    return true;
+}
+
+bool topo(){
+    memset(visit, 0, sizeof(visit));
+    memset(order, 0, sizeof(order));
+    t = n;
+    for(int i = 1; i <= n; i++){
+        if(!visit[i])
+            if(!dfs(i)) return false;
+    }
+    return true;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //file.open("output.txt");
+    while(cin >> n >> m, n != 0 || m != 0){
+        memset(graph, 0, sizeof(graph));
+        for(int i = 0; i < m; i++){
+            int a, b;
+            cin >> a >> b;
+            graph[a][b] = 1;
+        }
+        if(topo()){
+            cout << order[0];
+            for(int i = 1; i < n; i++) cout << ' ' << order[i];
+            cout << '\n';
+        }
+    }
+}
